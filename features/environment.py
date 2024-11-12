a2dc5c3b03ecdac60a3b485ebc48274b77ee09a1
ecahyo <ecahyono507@gmail.com>
2024-11-12 15:26:02 +0700

    fix error

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
-# from selenium.common.by import by
+from selenium.webdriver.chrome.options import Options
+import os
 
 def before_scenario(context,scenario):
+  option = Options()
+  option.add_argument("disable-gpu")
+  if os.getenv("HEADLESS") =='true':
+    option.add_argument("headless")
+    
   context.browser = webdriver.Chrome()
   context.browser.maximize_window()
 
