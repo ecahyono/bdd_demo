8e1b4ed2a740826706d406c4647b50d5e11c4e48
ecahyo <ecahyono507@gmail.com>
2024-11-12 15:36:19 +0700

    BDD

--- features/environment.py
+++ features/environment.py
@@ -4,11 +4,13 @@ import os
 
 def before_scenario(context,scenario):
   option = Options()
-  option.add_argument("disable-gpu")
+  option.add_argument("--disable-gpu")
+  option.add_argument("--no-sandbox")
+  option.add_argument("--disable-dev-shm-usage")
   if os.getenv("HEADLESS") =='true':
-    option.add_argument("headless")
-    
-  context.browser = webdriver.Chrome()
+    option.add_argument("--headless")
+
+  context.browser = webdriver.Chrome(options=options)
   context.browser.maximize_window()
 
 def after_scenario(context, scenario):
