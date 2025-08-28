5188eb70d8fb93c00ceab342881cf2798d7ab3d9
ecahyo <ecahyono507@gmail.com>
2025-08-28 12:45:09 +0700

    update features

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -5,34 +5,33 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @given(u'I am on the loggin page')
 def step_impl(context):
-    context.browser.get('https://frontend-lelang.lelang.dev.torche.id/login')
-    assert(context.browser.title) == "Lelang Indonesia | DJKN"
+    context.browser.get('https://katalon-demo-cura.herokuapp.com/profile.php#login')
+    assert(context.browser.title) == "CURA Healthcare Service"
 
 @given(u'I fill my credential')
 def step_impl(context):
-    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'username')))
-    context.browser.find_element(By.ID, 'username').send_keys('secondacount507@gmail.com') 
-    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'password')))
-    context.browser.find_element(By.ID, 'password').send_keys('Rahasi@123')
+    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'txt-username')))
+    context.browser.find_element(By.ID, 'txt-username').send_keys('John Doe') 
+    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'txt-password')))
+    context.browser.find_element(By.ID, 'txt-password').send_keys('ThisIsNotAPassword')
 
 @when(u'I am click Sign in Button')
 def step_impl(context):
-    context.browser.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/main/div[1]/div[2]/div/form/button').click()
+    context.browser.find_element(By.ID, 'btn-login').click()
 
 @then(u'I Shoud be Logged in')
 def step_impl(context):
-    WebDriverWait(context.browser, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"h3.text-xl"), 'Kategori Objek Lelang'))
-    assert(context.browser.find_element(By.CSS_SELECTOR,"h3.text-xl").text) =="Kategori Objek Lelang"
+    WebDriverWait(context.browser, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"#appointment>div>div>div>h2"), 'Make Appointment'))
+    assert(context.browser.find_element(By.CSS_SELECTOR,"#appointment>div>div>div>h2").text) =="Make Appointment"
     
 @given(u'I fill wrong credential')
 def step_impl(context):
-    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'username')))
-    context.browser.find_element(By.ID, 'username').send_keys('salah507@gmail.com') 
-    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'password')))
-    context.browser.find_element(By.ID, 'password').send_keys('salah@123')
+    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'txt-username')))
+    context.browser.find_element(By.ID, 'txt-username').send_keys('John Doe') 
+    WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.ID, 'txt-password')))
+    context.browser.find_element(By.ID, 'txt-password').send_keys('ThisIsWrongPassword')
 
 @then(u'I Shoud be Not Logged in and see the error message')
 def step_impl(context):
-    WebDriverWait(context.browser, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"span.text-center"), "Login gagal!"))
-    assert(context.browser.find_element(By.CSS_SELECTOR, "span.text-center.text-xl.font-bold.text-primary-500").text) =="Login gagal!"
-    assert(context.browser.find_element(By.CSS_SELECTOR, "span.text-center.text-sm.font-light.text-ternary-gray-200").text) =="Kredensial tidak valid."
+    WebDriverWait(context.browser, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"p.lead.text-danger"), "Login failed! Please ensure the username and password are valid."))
+    assert(context.browser.find_element(By.CSS_SELECTOR, "p.lead.text-danger").text) =="Login failed! Please ensure the username and password are valid."
